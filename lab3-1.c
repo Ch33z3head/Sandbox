c0f59b414f97b366c90400095c1a0089913c239b
Ch33z3head <77541959+Ch33z3head@users.noreply.github.com>
2023-10-27 09:32:05 -0500

    Add files via upload

---
+++ lab3-1.c
@@ -0,0 +1,26 @@
+/****************************************
+*       Lab 3.1.c                       *
+*       Jamie Grunewald                 *
+****************************************/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+//int main() {
+	int counter = 0;
+	int flag = 0;
+	char pswd = "thatwaseasy";
+    
+    scanf("%s", pswd);
+
+    //for (counter = 1; size= < 11; ++counter);
+  
+  // Success or not
+    if (flag != 0) 
+    {
+        printf("Corret\n");
+    }else{
+        printf("Incorrect\n");
+    }
+return 0;
+}
